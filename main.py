ee49ecb2d54292edadfe34343fbedcca79a6f671
MyD <m.demir-21@outlook.com>
2024-12-22 18:22:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 import logging
 from get_x_updates import get_x_updates
 from forward_message import forward_message
@@ -13,31 +13,29 @@ load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 # Botu başlatmak için gerekli ayarlar
-updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
 # Botu başlat
-updater.start_polling()
+application.run_polling()
 
 # '/start' komutu
-def start(update, context):
-    update.message.reply_text("Bot çalışıyor! X (Twitter) kullanıcı adı girerek güncellemeleri alabilirsiniz.")
+async def start(update: Update, context):
+    await update.message.reply_text("Bot çalışıyor! X (Twitter) kullanıcı adı girerek güncellemeleri alabilirsiniz.")
 
 # '/get_updates' komutu, X (Twitter) güncellemelerini almak için
-def get_updates(update, context):
+async def get_updates(update: Update, context):
     if len(context.args) == 0:
-        update.message.reply_text("Lütfen bir X (Twitter) kullanıcı adı girin.")
+        await update.message.reply_text("Lütfen bir X (Twitter) kullanıcı adı girin.")
         return
 
     username = context.args[0]
     updates = get_x_updates(username)
-    update.message.reply_text(updates)
+    await update.message.reply_text(updates)
 
-# Mesajı iletmek için mesaj işleyici
-dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('get_updates', get_updates))
-dispatcher.add_handler(MessageHandler(Filters.text, forward_message))
+# Komutları ekleyelim
+application.add_handler(CommandHandler('start', start))
+application.add_handler(CommandHandler('get_updates', get_updates))
+application.add_handler(MessageHandler(filters.TEXT, forward_message))
 
 # Loglama yapılandırması
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
