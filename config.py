33f3bdf3166e740aeadef3cc47b08401e908677a
MyD <m.demir-21@outlook.com>
2024-12-22 23:23:48 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-def load_env():
-    """.env dosyasındaki bilgileri yükler"""
-    load_dotenv()
-
-# Telegram Bot Token'ı
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
