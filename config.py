03d272de3d600e0817668ed0d025e9a5f100d4da
MyD <m.demir-21@outlook.com>
2024-12-22 15:40:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-# Telegram'da mesajları yönlendireceğiniz hedef kanallar
-TARGET_CHANNELS = ['@channel1', '@channel2', '@channel3']
+TELEGRAM_BOT_TOKEN = "7520250082:AAGvNgm1KyT6-lmONaHvRKusMKhX3sGp-pM"
+X_USERNAME = "elonmusk"
+TARGET_CHANNELS = ["@target_channel1", "@target_channel2"]
