3f1560be17a1e784a01028b85478f6d9d7e428fa
MyD <m.demir-21@outlook.com>
2024-12-22 20:09:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-from telegram_bot import start, set_source_group, set_target_group, set_twitter_target, forward_twitter_updates
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from telegram_bot import (
+    start,
+    set_source_group,
+    set_target_group,
+    set_twitter_target,
+    forward_twitter_updates,
+)
 from bot.config import TELEGRAM_BOT_TOKEN
 
 def main():
-    updater = Updater(TELEGRAM_BOT_TOKEN)
+    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
-    dp = updater.dispatcher
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, set_source_group))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, set_target_group))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, set_twitter_target))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_twitter_updates))
 
-    # Komutlar ve mesaj işleyicilerinin eklenmesi
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, set_source_group))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, set_target_group))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, set_twitter_target))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, forward_twitter_updates))
-
-    # Botun çalıştırılması
-    updater.start_polling()
-    updater.idle()
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
