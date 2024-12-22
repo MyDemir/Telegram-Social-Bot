417788701cd6ca17f417c04adc15e8090889e29c
MyD <m.demir-21@outlook.com>
2024-12-23 02:31:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
+import os
+from dotenv import load_dotenv
+from telegram_bot import start, set_channels, forward_messages
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+
+# .env dosyasını yükleme
+load_dotenv()
+
+# TELEGRAM_BOT_TOKEN çevresel değişkeninden bot tokenini al
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
+if not TELEGRAM_BOT_TOKEN:
+    raise ValueError("Bot tokeni .env dosyasından alınamadı. Lütfen TELEGRAM_BOT_TOKEN değeri ekleyin.")
+
+def main() -> None:
+    # Telegram botunun token'ını .env dosyasından alıyoruz
+    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+
+    # Handler'lar
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("set_channels", set_channels))
+    
+    # Text mesajlarını alıyoruz, fakat komutları hariç tutuyoruz
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_messages))
+
+    # Botu çalıştır
+    application.run_polling()
+
+if __name__ == "__main__":
+    main()
+
 """import os
 from dotenv import load_dotenv
 from telegram_bot import start, set_channels, forward_messages, forward_twitter_updates
@@ -31,7 +62,9 @@ def main() -> None:
 if __name__ == "__main__":
     main()
     """
-import os
+
+
+"""import os
 from dotenv import load_dotenv
 from telegram_bot import start, forward_messages
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
@@ -60,3 +93,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+"""
