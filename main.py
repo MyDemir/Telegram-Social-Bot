b2ff4fadbd93b4db8b9b4da124a497aa49ca2216
MyD <m.demir-21@outlook.com>
2024-12-22 23:12:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from telegram_bot import start, set_channels, forward_messages, forward_twitter_updates
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
-import asyncio
 
 # .env dosyasını yükleme
 load_dotenv()
@@ -20,18 +19,13 @@ async def main() -> None:
     # Handler'lar
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("set_channels", set_channels))
-    
-    # Text mesajlarını, ancak komutları dışarıda bırakacak şekilde ekliyoruz
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_messages))
-
-    # Twitter güncellemeleri iletme komutunu ekliyoruz
+    application.add_handler(MessageHandler(filters.Text & ~filters.Command, forward_messages))
     application.add_handler(CommandHandler("forward_twitter_updates", forward_twitter_updates))
 
     # Botu çalıştır
     await application.run_polling()
 
 if __name__ == "__main__":
-    # asyncio.run yerine doğrudan asyncio event loop ile çalışıyoruz
-    loop = asyncio.get_event_loop()
-    loop.create_task(main())
-    loop.run_forever()
+    # asyncio.run(main()) yerine direkt application.run_polling() kullanıyoruz
+    import asyncio
+    asyncio.run(main())
