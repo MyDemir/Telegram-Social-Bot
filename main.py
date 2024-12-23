60ea2cf6932456b1657605b206868c49fce8d790
MyD <m.demir-21@outlook.com>
2024-12-23 16:28:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,22 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 # .env dosyasını yükleme
 load_dotenv()
 
-# TELEGRAM_BOT_TOKEN çevresel değişkeninden bot tokenini al
+# Bot tokenini yükleme
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
 if not TELEGRAM_BOT_TOKEN:
-    raise ValueError("Bot tokeni .env dosyasından alınamadı. Lütfen TELEGRAM_BOT_TOKEN değeri ekleyin.")
+    raise ValueError("Bot tokeni bulunamadı. Lütfen .env dosyasına TELEGRAM_BOT_TOKEN ekleyin.")
 
 def main() -> None:
-    # Telegram botunun token'ını .env dosyasından alıyoruz
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
-    # Handler'lar
+    # Komutlar
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("set_channels", set_channels))
-    
-    # Text ve medya mesajlarını alıyoruz, fakat komutları hariç tutuyoruz
+
+    # Kanal mesajlarını dinleme
     application.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, forward_content))
 
-    # Botu çalıştır
     application.run_polling()
 
 if __name__ == "__main__":
