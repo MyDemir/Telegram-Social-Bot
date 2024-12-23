d8c5b7c184294e15db86cb18cebf0d016794a84d
MyD <m.demir-21@outlook.com>
2024-12-23 20:45:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 import os
-from telegram import Update
 from dotenv import load_dotenv
-from telegram.ext import Application, CommandHandler
-from telegram_bot import start, set_channels, forward_content, notify_twitter_update, set_twitter
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from telegram_bot import start, set_channels, set_twitter, send_channel_update_notification
 
 def main():
-    # .env dosyasını yükleme
     load_dotenv()
 
-    # Bot tokenini yükleme
     TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-
     if not TELEGRAM_BOT_TOKEN:
-        raise ValueError("Bot tokeni bulunamadı. Lütfen .env dosyasına TELEGRAM_BOT_TOKEN ekleyin.")
-    
+        raise ValueError("Bot tokeni bulunamadı. .env dosyasına TELEGRAM_BOT_TOKEN ekleyin.")
+
     application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
     # Komutları ekle
-    application.add_handler(CommandHandler("start", start))  # Başlat komutu
-    application.add_handler(CommandHandler("set_channels", set_channels))  # Kanal ayarları
-    application.add_handler(CommandHandler("set_twitter", set_twitter))  # Twitter kullanıcı adını ayarlama
-    application.add_handler(CommandHandler("notify_twitter_update", notify_twitter_update))  # Twitter bildirimleri
-    application.add_handler(CommandHandler("forward_content", forward_content))  # Kanal içeriği yönlendirme
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("set_channels", set_channels))
+    application.add_handler(CommandHandler("set_twitter", set_twitter))
 
-    # Uygulamayı başlat
+    # Kanal mesaj bildirimleri
+    application.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, send_channel_update_notification))
+
     application.run_polling()
 
 if __name__ == "__main__":
