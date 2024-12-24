88cbff4253bf3ad69e5057d4b781983be92a134d
MyD <m.demir-21@outlook.com>
2024-12-24 03:06:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from telegram_bot import start, set_channels, forward_content
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+
+# .env dosyasını yükleme
+load_dotenv()
+
+# Bot tokenini yükleme
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
+if not TELEGRAM_BOT_TOKEN:
+    raise ValueError("Bot tokeni bulunamadı. Lütfen .env dosyasına TELEGRAM_BOT_TOKEN ekleyin.")
+
+def main() -> None:
+    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+
+    # Komutlar
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("set_channels", set_channels))
+
+    # Kanal mesajlarını dinleme
+    application.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, forward_content))
+
+    application.run_polling()
+
+if __name__ == "__main__":
+    main()
