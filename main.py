7118c11421482736f815b519f513b1b903abc23f
MyD <m.demir-21@outlook.com>
2024-12-28 00:30:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from telegram_bot import start, set_channels, forward_content
+from telegram_bot import start, set_channels, forward_content, add_twitter_user
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
 # .env dosyasını yükleme
@@ -18,6 +18,7 @@ def main() -> None:
     # Komutlar
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("set_channels", set_channels))
+    application.add_handler(CommandHandler("add_twitter", add_twitter_user))  # Twitter kullanıcı ekleme komutu
 
     # Kanal mesajlarını dinleme
     application.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, forward_content))
